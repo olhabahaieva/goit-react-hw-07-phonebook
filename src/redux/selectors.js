f691cf3b035691805a5b91ba6abc4c68b54408a8
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-28 23:47:56 +0300

    added visible filters contacts selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,8 @@
 // import { createSelector } from "@reduxjs/toolkit";
 // import { useSelector } from "react-redux";
 
+import { createSelector } from "@reduxjs/toolkit";
+
 // import { createSelector } from "@reduxjs/toolkit";
 
 export const selectContacts = state => state.contacts.items;
@@ -9,13 +11,23 @@ export const selectIsLoading = state => state.contacts.items;
 
 export const selectError = state => state.contacts.error;
 
+export const selectStatusFilter = state => state.contacts.items;
+
 export const getContacts = (store) => {
     return store.contacts;
   };
-  // export const selectVisibleContacts = createSelector(
-  //   [selectContacts],
-  //   (contacts) => {
-     
-  //     return contacts;
-  //   }
-  // );
+
+  export const selectVisibleContacts = createSelector(
+    [selectContacts, selectStatusFilter],
+    (contacts, statusFilter) => {
+     switch (statusFilter){
+      case statusFilter.active:
+        return contacts.filter(contact => contact.completed);
+      case statusFilter.completed:
+        return contacts.filter(contact => contact.completed);
+        default:
+          return contacts;
+     }
+      
+    }
+  );
